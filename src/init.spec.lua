fe3f145a6d16e3e730bca76946d6285ed2d4b66c
AmaranthineCodices <AmaranthineCodices@gmail.com>
2018-03-14 19:59:05 -0400

    expand test coverage

--- src/init.spec.lua
+++ src/init.spec.lua
@@ -20,5 +20,45 @@ return function()
                 Selector.createSelector(print, 1, warn)
             end)
         end)
+
+        it("selectors should return a value", function()
+            local testSource = function()
+                return 1
+            end
+
+            local testSelector = Selector.createSelector(testSource, function(value1)
+                return value1 * 2
+            end)
+
+            local value = testSelector()
+            expect(value).to.equal(2)
+        end)
+
+        it("selectors should receive their arguments in order of declaration", function()
+            local function a()
+                return 1
+            end
+
+            local function b()
+                return 2
+            end
+
+            local function c()
+                return 3
+            end
+
+            local value1, value2, value3
+
+            local selector = Selector.createSelector(a, b, c, function(v1, v2, v3)
+                value1 = v1
+                value2 = v2
+                value3 = v3
+            end)
+
+            selector()
+            expect(value1).to.equal(1)
+            expect(value2).to.equal(2)
+            expect(value3).to.equal(3)
+        end)
     end)
 end
